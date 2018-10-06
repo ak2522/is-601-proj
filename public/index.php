a11ef3ebe39a9a352a7d404d5cd3b0a3edfff73c
anupama <ak2522@njit.edu>
2018-10-06 15:57:02 -0400

    Task: Added class csv,html and system to hold the data for reading the csv file, generate table and print data

--- public/index.php
+++ public/index.php
@@ -1,4 +1,32 @@
 <?php
-echo "test";
-echo "This is my first project";
-echo "Hello all";
+main::start();
+class main
+{
+    static public function start($filename)
+    {
+        $records = csv::getRecords($filename);
+        $table = html::build_table($records);
+        system:: Printpage($table);
+    }
+
+}
+
+class csv
+{
+    static public function getRecords($filename)
+    {}
+}
+
+class html
+{
+    public static function build_table($array)
+    {}
+}
+
+class system{
+    static public function Printpage($page)
+    {
+        echo $page;
+    }
+
+}
