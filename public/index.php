a6895b6e0ee6372e5acf4dffd4ae8bd132a9229d
anupama <ak2522@njit.edu>
2018-10-06 17:03:00 -0400

    Feature: Added bootstrap table header

--- public/index.php
+++ public/index.php
@@ -72,8 +72,21 @@ class html
 {
     public static function build_table($array)
     {
+        //start a table
+        echo "<html lang=\"en\">
+            <head>
+            <meta charset=\"utf-8\">
+            <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+            <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\">
+            <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>
+            <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script>
+            </head>
+            </html>";
+
         $html = '<table class="table table-striped">';
         $html .= '<tr>';
+
+        //header rows
         foreach ($array[0] as $key => $value) {
             $html .= '<th>' . htmlspecialchars($key) . '</th>';
         }
